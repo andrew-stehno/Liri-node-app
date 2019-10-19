887c281180150fe733cb0e3a1f601637a52f35c8
andrew stehno <andrew_stehno@yahoo.com>
2019-10-19 12:48:40 -0600

    spotify api call is functioning

--- liri.js
+++ liri.js
@@ -1,3 +1,19 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+
 var spotify = new Spotify(keys.spotify);
+var trackTitle = process.argv[2];
+
+spotify
+    .search({
+        type: 'track', query: trackTitle, limit: 1
+    })
+    .then(function(response) {
+        console.log(response.tracks.items);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
